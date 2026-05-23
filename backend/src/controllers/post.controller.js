1840eb6337bbee4fc281f8c9d10f62bbb8994160
mayanksrivastava922 <catvideos999yt@gmail.com>
2026-05-23 18:15:47 +0530

    post details , create , get added

---
+++ backend/src/controllers/post.controller.js
@@ -0,0 +1,60 @@
+import ImageKit, { toFile } from "@imagekit/nodejs"
+import postModel from "../models/post.model.js";
+
+const imagekit = new ImageKit({
+    privateKey: process.env.IMAGEKIT_PUBLIC_KEY
+})
+
+export const createPostController = async (req, res) => {
+
+    const BufferFile = req.file;
+    const caption = req.body.caption;
+
+    if (!caption) return res.status(400).json({ message: "Please Write Caption" })
+    if (!BufferFile) return res.status(400).json({ message: "Please Select Post" })
+
+    //user id taken from token 
+    const token = req.user._id
+
+    const file = await imagekit.files.upload({
+        file: await toFile(Buffer.from(BufferFile.buffer), "file"),
+        fileName: "file"
+    })
+
+    const Post = await postModel.create({
+        caption: caption,
+        image: file.url,
+        author: token
+    })
+
+
+    return res.status(201).json({ message: "Post Created Successfully", file, Post })
+}
+
+export const getPostController = async (req, res) => {
+    const userId = req.user._id
+
+    const post = await postModel.find({
+        author: userId
+    })
+
+    if (post.length < 1) return res.status(200).json({ message: "No Post Unvailable" });
+    return res.status(200).json({ message: "Post Fetch Successfully", post })
+}
+export const detailsPostController = async (req, res) => {
+    const postId = req.params.postId;
+    if(!postId) return res.status(400).json({ message: "Post Id Required" })
+
+    const userId = req.user._id;
+    if(!userId) return res.status(400).json({ message: "User Id Required" })
+
+    const Post = await postModel.findById(postId)
+    if (!Post) return res.status(404).json({ message: "Post Not Found" })
+
+    const CheckUserBelongsPost = Post.author.toString() === userId.toString()
+
+    if (!CheckUserBelongsPost) return res.status(403).json({ message: "Forbidden Content" })
+
+    return res.status(200).json({ mesage: "post founded", Post })
+
+}
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,5 +1,31 @@
 import { ENV } from "../config/env.js"
+import jwt from "jsonwebtoken"
+import userModel from "../models/auth.model.js";
 
-export const authMiddleware = async (req, res) => {
+export const protectedRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+
+        if (!token) return res.status(401).json({ message: "Unauthorized , No Token Provided" })
+
+        const decoded = jwt.verify(token, ENV.JWT_SECRET)
+
+        if (!decoded) return res.status(401).json({ message: "Invalid User Token " })
+
+        const user = await userModel.findById(decoded.userId).select("-password");
+
+        if (!user) return res.status(401).json({ message: "User Not Found" })
+
+        req.user = user;
+
+        next()
+    }
+    catch (error) {
+        console.log(error);
+
+        return res.status(401).json({
+            message: error.message
+        });
+    }
 
 }
